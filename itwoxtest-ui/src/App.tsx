82ba050c261d3af25e9ee43b1dffcd15281d47a4
Prathamesh Dutondkar <prathamesh.dutondkar@perpetualblock.io>
2023-10-07 15:11:40 +0530

    Add Navbar and react routering in project

--- itwoxtest-ui/src/App.tsx
+++ itwoxtest-ui/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import './App.css';
+import Dashboard from './views/Dashboard/Dashboard';
+import LandingPage from './views/LandingPage.js/landingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signin" element={<LandingPage />} />
+          <Route path="/dashboard" element={<LandingPage />} />
+
+
+          <Route path="/aboutus" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ itwoxtest-ui/src/views/LandingPage.js/landingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./landingpage.scss"
+
+
+
+const LandingPage = () => {
+  return (
+    <div>
+    Landing Page
+
+      
+    </div>
+  )
+}
+
+export default LandingPage;
