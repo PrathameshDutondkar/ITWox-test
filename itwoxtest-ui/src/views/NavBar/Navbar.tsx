70e9022ec5dd2d2f2b8660e75eb8a0955f58f91e
Prathamesh Dutondkar <prathamesh.dutondkar@perpetualblock.io>
2023-10-08 19:52:07 +0530

    Add test for navbar

--- itwoxtest-ui/src/views/NavBar/Navbar.tsx
+++ itwoxtest-ui/src/views/NavBar/Navbar.tsx
@@ -1,49 +1,49 @@
-import React, { useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import logo from "../../assets/it-wox-logo.svg";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "./navbar.scss";
+  import React, { useEffect } from "react";
+  import { Link, useLocation, useNavigate } from "react-router-dom";
+  import logo from "../../assets/it-wox-logo.svg";
 
-const Navbar = () => {
-  const email = localStorage.getItem("user");
-  const location = useLocation();
-  const navigate = useNavigate();
+  import "react-toastify/dist/ReactToastify.css";
+  import "./navbar.scss";
 
-  const handleSignOut = () => {
-    localStorage.removeItem("user");
-    
-    navigate("/");
+  const Navbar = () => {
+    const email = localStorage.getItem("user");
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleSignOut = () => {
+      localStorage.removeItem("user");
+      
+      navigate("/");
+    };
+
+    const shouldShowNavbar = location.pathname !== "/signin";
+
+    useEffect(() => {
+      if (location.pathname === "/") {
+        handleSignOut();
+      }
+    }, [location.pathname]);
+
+    return shouldShowNavbar ? (
+      <nav className="navbar-container">
+        <span>
+          <img src={logo} alt="IT WOX Logo" />
+        </span>
+        <span className="menu-container">
+          <li key="Get help with research" className="tab-li">
+            {email ? (
+              <button onClick={handleSignOut} className="sign-out-button">
+                Sign Out
+              </button>
+            ) : (
+              <Link to="/signin">
+                <button className="sign-in-button">Sign In</button>
+              </Link>
+            )}
+          </li>
+        </span>
+      </nav>
+    ) : null;
   };
 
-  const shouldShowNavbar = location.pathname !== "/signin";
-
-  useEffect(() => {
-    if (location.pathname === "/") {
-      handleSignOut();
-    }
-  }, [location.pathname]);
-
-  return shouldShowNavbar ? (
-    <nav className="navbar-container">
-      <span>
-        <img src={logo} alt="IT WOX Logo" />
-      </span>
-      <span className="menu-container">
-        <li key="Get help with research" className="tab-li">
-          {email ? (
-            <button onClick={handleSignOut} className="sign-out-button">
-              Sign Out
-            </button>
-          ) : (
-            <Link to="/signin">
-              <button className="sign-in-button">Sign In</button>
-            </Link>
-          )}
-        </li>
-      </span>
-    </nav>
-  ) : null;
-};
-
-export default Navbar;
+  export default Navbar;
