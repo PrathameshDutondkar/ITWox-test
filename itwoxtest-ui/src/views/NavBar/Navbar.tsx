b8f894a182da20f5bceec10f1d575b4294778748
Prathamesh Dutondkar <prathamesh.dutondkar@perpetualblock.io>
2023-10-08 07:08:06 +0530

    Remove Navbar on singin route

--- itwoxtest-ui/src/views/NavBar/Navbar.tsx
+++ itwoxtest-ui/src/views/NavBar/Navbar.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./navbar.scss";
-import { Link, useNavigate } from "react-router-dom";
-import logo from "../../assets/it-wox-logo.svg"
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import logo from "../../assets/it-wox-logo.svg";
 
 const Navbar = () => {
   const email = localStorage.getItem("user");
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleSignOut = () => {
     localStorage.removeItem("user");
     navigate("/");
   };
+
+  // Conditionally render the Navbar based on the route
+  if (location.pathname === "/signin") {
+    return null; // Hide the Navbar on the /signin route
+  }
 
   return (
     <div className="navbar-container">
